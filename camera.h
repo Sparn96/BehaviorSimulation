669a893e504c8ed8ba75358130530a9d57026f30
Sparn96 <Sparn96@users.noreply.github.com>
2017-02-01 17:55:30 -0800

    Update camera.h

--- camera.h
+++ camera.h
@@ -4,13 +4,13 @@
 class Camera
 {
 public:
-Camera();
+Camera() {x_pos = 0; y_pos = 0;}
 ~Camera();
 Camera(Camera&);
 
 void setPos(float x, float y) { x_pos = x; y_pos = y; return;}
 void getPostion(float& x, float& y) {x = x_pos; y = y_pos; return;}
-//move function?  NICK?
+//move function -- not for now, the camera will encomapss our entire game map (which will be very small)
 
 private:
 
